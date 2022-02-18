f0612b454c88d4fc6e02c20e8d310b999e13c2d8
Andrey Vazhenov <andrejvazenov@gmail.com>
2022-02-18 17:16:49 +0700

    Created a loop with a timer inside the loop to display title characters one by one

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,22 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
-    }
+        
+        var charIndex = 0.0
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        for letter in titleText { // создаем цикл и таймер в нем, чтобы буквы заголовка появлялись на экране поочередно с задержкой
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+        }
     
-
+    
 }
