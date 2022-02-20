dff1788dce7697578ff5e4e214bcf008ae445875
Andrey Vazhenov <andrejvazenov@gmail.com>
2022-02-20 21:54:12 +0700

    Добавил FireBase через Cocoa Pods

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
+
 class WelcomeViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+  
         
         var charIndex = 0.0
         titleLabel.text = ""
@@ -24,6 +27,8 @@ class WelcomeViewController: UIViewController {
             }
             charIndex += 1
         }
+        
+        
         }
     
     
