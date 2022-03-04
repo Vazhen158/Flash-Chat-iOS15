72e255e1344a0c4c33c186cb95acf6801e25fca0
Andrey Vazhenov <andrejvazenov@gmail.com>
2022-03-04 15:13:27 +0700

    реализовал сортировку сообщений по дате отправки

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -17,7 +17,7 @@ class ChatViewController: UIViewController {
     let db = Firestore.firestore()
     
     var messages: [Message] = [
-       
+        
     ]
     
     
@@ -35,32 +35,37 @@ class ChatViewController: UIViewController {
         
     }
     
-    func loadMessages() {
-       
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
-            self.messages = []
-            if let e = error {
-                print("Возникла проблема при получении данных из FireStore, \(e)")
-            } else {
-                if let snapshotDocuments = querySnapshot?.documents {
-                    for doc in snapshotDocuments {
-                        let data = doc.data()
-                        if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
-                            let newMessage = Message(sender: messageSender, body: messageBody)
-                            self.messages.append(newMessage)
-                            DispatchQueue.main.async {
-                                self.tableView.reloadData()
+    
+    func loadMessages() { // метод для получения данных из бд и отображения на экране в реальном времени
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+                self.messages = []
+                if let e = error {
+                    print("Возникла проблема при получении данных из FireStore, \(e)")
+                } else {
+                    if let snapshotDocuments = querySnapshot?.documents {
+                        for doc in snapshotDocuments {
+                            let data = doc.data()
+                            if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                                let newMessage = Message(sender: messageSender, body: messageBody)
+                                self.messages.append(newMessage)
+                                DispatchQueue.main.async {
+                                    self.tableView.reloadData()
+                                }
                             }
                         }
                     }
                 }
             }
-        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        //  сохранение данных в бд
         if  let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { (error) in
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender,
+                                                                      K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
                 if let e = error {
                     print("При сохранении данных в FireStore возникла проблема, \(e)")
                 } else {
@@ -83,6 +88,7 @@ class ChatViewController: UIViewController {
     
 }
 
+
 extension ChatViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return  messages.count
@@ -91,7 +97,6 @@ extension ChatViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         cell.label.text = messages[indexPath.row].body
-        
         return cell
     }
 }
